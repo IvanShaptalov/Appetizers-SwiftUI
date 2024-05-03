73c39f94df152e3f826a2512bb725b2e8c04bf4f
IvanShaptalov <ivan.shaptalov1@gmail.com>
2024-05-03 11:02:07 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by PowerMac on 03.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
